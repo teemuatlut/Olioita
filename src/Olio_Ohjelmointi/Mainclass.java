d3572fd8c839b5d0f1955b6d18f9744ad1eaa341
teemuatlut <teemu.mantykallio@live.fi>
2015-09-23 11:42:15 +0300

    First commit for Hello World

---
+++ src/Olio_Ohjelmointi/Mainclass.java
@@ -0,0 +1,18 @@
+/*
+ * Mainclass.java
+ * LUT Olio-ohjelmointi
+ * Prints hello world
+ */
+
+/**
+ *
+ * @author Teemu Mäntykallio, 0371460
+ */
+//package Olio_Ohjelmointi;
+
+public class Mainclass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+}
