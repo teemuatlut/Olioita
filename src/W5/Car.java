1c73f0bf6513cd6112700cdf8052751d8224e18e
teemuatlut <teemu.mantykallio@live.fi>
2015-11-11 09:23:42 +0200

    Merge origin/master
    Conflicts:
    	src/W2/Dog.java
    	src/W2/Mainclass.java

---
+++ src/W5/Car.java
@@ -0,0 +1,30 @@
+/*
+ * Car.java
+ * LUT Olio-ohjelmointi | 28.10.2015
+ * 
+ * Author: Teemu M�ntykallio
+ */
+package W5;
+
+/**
+ *
+ * @author Teemu
+ */
+public class Car {
+    public Car(){
+        System.out.print("Valmistetaan: ");
+        buildBody();
+        System.out.print("Valmistetaan: ");
+        buildChassis();
+        System.out.print("Valmistetaan: ");
+        buildEngine();
+        System.out.print("Valmistetaan: ");
+        buildWheel();
+        System.out.print("Valmistetaan: ");
+        buildWheel();
+        System.out.print("Valmistetaan: ");
+        buildWheel();
+        System.out.print("Valmistetaan: ");
+        buildWheel();
+    }
+}
---
+++ src/W5/Mainclass.java
@@ -0,0 +1,17 @@
+/*
+ * Mainclass.java
+ * LUT Olio-ohjelmointi | 28.10.2015
+ * 
+ * Author: Teemu M�ntykallio
+ */
+package W5;
+
+/**
+ *
+ * @author Teemu
+ */
+public class Mainclass {
+    public static void main(String[] args) {
+        
+    }
+}
