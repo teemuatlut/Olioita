d25dd4556dd235b59e4a71c38ee75369397f5197
teemuatLUT <teemu.mantykallio@live.fi>
2016-10-01 12:24:52 +0300

    W1 changes. Nothing changed

--- src/Olio_Ohjelmointi/Mainclass.java
+++ src/Olio_Ohjelmointi/Mainclass.java
@@ -2,17 +2,13 @@
  * Mainclass.java
  * LUT Olio-ohjelmointi
  * Prints hello world
- */
-package Olio_Ohjelmointi;
-/**
  *
  * @author Teemu Mäntykallio, 0371460
  */
-//package Olio_Ohjelmointi;
+package Olio_Ohjelmointi;
 
 public class Mainclass {
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hello World!");
     }
 }
